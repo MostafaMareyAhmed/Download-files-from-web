4fd7b11156587dbb5a657e74f91478646a767ed2
MostafaMareyAhmed <mostafa.mareyahmed@gmail.com>
2021-07-31 04:24:12 +0200

    Download files from web

--- config.py
+++ config.py
@@ -1,13 +1,3 @@
 videos_urls = [
-    "https://cdn.fs.teachablecdn.com/P4xBUDaITVeRUiUjhDKp",
-    "https://cdn.fs.teachablecdn.com/1KMU00STwqf3oTahcA6P",
-    "https://cdn.fs.teachablecdn.com/VFMWbOxSR3et6uM2MEM0",
-    "https://cdn.fs.teachablecdn.com/VkDGrHUxQH2iNu8gWskM",
-    "https://cdn.fs.teachablecdn.com/2q2cd7iQAqY2bNSy3avR",
-    "https://cdn.fs.teachablecdn.com/r349laGpQbGaruPvnOBF",
-    "https://cdn.fs.teachablecdn.com/YU45tYXREKZ7Wimz5WyQ",
-    "https://cdn.fs.teachablecdn.com/bEw6PdPERAOacRnrW6dM",
-    "https://cdn.fs.teachablecdn.com/yObRI3jGToGZDBr3JBJ4",
-    "https://cdn.fs.teachablecdn.com/gDGJGuLCQdqGxtSEcaMj",
-    "https://cdn.fs.teachablecdn.com/ehVgGZSDQEyMfnEM3nZ6"
+    
 ]
